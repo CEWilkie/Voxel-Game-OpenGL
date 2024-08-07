2b388e0811cddce1a073d6eed5547044da8da76c
MisterPuggs <callum.wilkie2023@my.ntu.ac.uk>
2024-08-07 22:12:03 +0100

    .

--- src/Player/Player.h
+++ src/Player/Player.h
@@ -16,39 +16,6 @@ enum class MOVEMENTMODE {
         WALKING, FLYING, // ...
 };
 
-
-struct Kinematics {
-    glm::vec3 initPosition {};          //          // initial position movement starts from (NOT DISPLACEMENT)
-    glm::vec3 initVelocity {};          // u        // velocity at start of a movement
-    glm::vec3 finalVelocity {};         // v        // velocity at end of a movement
-    glm::vec3 acceleration {};          // a        // acceleration of velocity
-    float seconds;                      // t        // time measurement for equations
-
-    // Set initial values for an entity's movement, assumes s = 0, t = 0;
-    void StartMovement(glm::vec3 _p, glm::vec3 _u, glm::vec3 _a) {
-        initPosition = _p;
-        initVelocity = _u;
-        finalVelocity = {0,0,0};
-        acceleration = _a;
-        seconds = 0;
-    }
-
-    // Update the values being used in the movement without restarting the movement time
-    void UpdateMovementValues(glm::vec3 _p, glm::vec3 _u, glm::vec3 _a) {
-        initPosition = _p;
-        initVelocity = _u;
-        acceleration = _a;
-    }
-
-    // returns the new position modeled from init position of the current movement's u, a, and sum time t
-    glm::vec3 GetNewPosition(float _seconds) {
-        seconds += _seconds; // sum of seconds in jump
-        finalVelocity = initVelocity + acceleration * seconds; // v = u + at
-        return (initVelocity * seconds) + (0.5f * acceleration * std::pow(seconds, 2.0f)) + initPosition; // new p = ut + 0.5a(t^2) + p
-    }
-
-};
-
 class Player {
     private:
         // Positioning info
